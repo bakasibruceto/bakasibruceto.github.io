dca78de4df57e8835370fb26255e72d7d0ed3a3b
bakasibruceto <abla09027@gmail.com>
2023-12-17 16:52:27 +0800

    add new images

--- dist/app.js
+++ dist/app.js
@@ -1,87 +1,118 @@
-// anime({
-//     targets: '.class8',
-//     keyframes: [
-//         {translateX: 0, translateY: 0, rotate: 0},
-//         {translateX: 0, translateY: -50, rotate: -30},
-//     ],
-//     duration: 1000,
-//     easing: 'easeInOutSine',
-//     direction: 'alternate',
-//     loop: true
-// });
+window.onload = function () {
+    anime({
+        targets: '#imageContainer',
+        opacity: [0, 1],
+        duration: 1000,
+        easing: 'easeInOutQuad'
+    });
+    anime({
+        targets: '.class3',
+        keyframes: [
+            { translateX: -20, rotate: 8 },
+            { translateX: 0, rotate: 0 }
+        ],
+        duration: 10000,
+        easing: 'easeInOutSine',
+        direction: 'alternate',
+        loop: true
+    });
+    anime({
+        targets: '.class4',
+        keyframes: [
+            { translateX: -40, rotate: 16 },
+            { translateX: 0, rotate: 0 }
+        ],
+        duration: 11000,
+        easing: 'easeInOutSine',
+        direction: 'alternate',
+        loop: true
+    });
 
-//left_bang
-anime({
-    targets: '.class6',
-    keyframes: [
-        {translateX: -25, rotate: 10},
-        {translateX: 0, rotate: 0},
-    ],
-    duration: 10000,
-    easing: 'easeInOutSine',
-    direction: 'alternate',
-    loop: true
-});
+    anime({
+        targets: '.class5',
+        keyframes: [
+            { translateX: -25, rotate: 10 },
+            { translateX: 0, rotate: 0 },
+        ],
+        duration: 9900,
+        easing: 'easeInOutSine',
+        direction: 'alternate',
+        loop: true
+    });
 
-//front_bang_left
-anime({
-    targets: '.class3',
-    keyframes: [
-        {translateX: -25, rotate: 10},
-        {translateX: 0, rotate: 0},
-    ],
-    duration: 9800,
-    easing: 'easeInOutSine',
-    direction: 'alternate',
-    loop: true
-});
+    anime({
+        targets: '.class6',
+        keyframes: [
+            { translateX: 10, rotate: -5 },
+            { translateX: -10, rotate: 5 },
 
-//front_bang_right
-anime({
-    targets: '.class4',
-    keyframes: [
-        {translateX: -25, rotate: 10},
-        {translateX: 25, rotate: -10},
-    ],
-    duration: 9800,
-    easing: 'easeInOutSine',
-    direction: 'alternate',
-    loop: true
-});
-//front_bang_right2
-anime({
-    targets: '.class5',
-    keyframes: [
-        {translateX: -25, rotate: 10},
-        {translateX: 25, rotate: 10},
-    ],
-    duration: 9800,
-    easing: 'easeInOutSine',
-    direction: 'alternate',
-    loop: true
-});
-//middle_front_bang
-anime({
-    targets: '.class9',
-    keyframes: [
-        {translateX: -25, rotate: 10},
-        {translateX: 25, rotate: -10},
-    ],
-    duration: 9900,
-    easing: 'easeInOutSine',
-    direction: 'alternate',
-    loop: true
-});
+        ],
+        duration: 11000,
+        easing: 'easeInOutSine',
+        direction: 'alternate',
+        loop: true
+    });
 
-anime({
-    targets: '.class5',
-    keyframes: [
-        {translateX: -25, rotate: 10},
-        {translateX: 25, rotate: -10},
-    ],
-    duration: 9900,
-    easing: 'easeInOutSine',
-    direction: 'alternate',
-    loop: true
-});
+    anime({
+        targets: '.class7',
+        keyframes: [
+            { translateX: 25, rotate: -10 },
+            { translateX: 20, rotate: -8 },
+
+        ],
+        duration: 9800,
+        easing: 'easeInOutSine',
+        direction: 'alternate',
+        loop: true
+    });
+
+    anime({
+        targets: '.class8',
+        keyframes: [
+            { translateX: 10, rotate: -5 },
+            { translateX: -10, rotate: 5 },
+
+        ],
+        duration: 9800,
+        easing: 'easeInOutSine',
+        direction: 'alternate',
+        loop: true
+    });
+
+    anime({
+        targets: '.class9',
+        keyframes: [
+            { translateX: 30, rotate: -12 },
+            { translateX: 0, rotate: 0 },
+
+
+        ],
+        duration: 9800,
+        easing: 'easeInOutSine',
+        direction: 'alternate',
+        loop: true
+    });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- dist/assets.js
+++ dist/assets.js
@@ -1,26 +1,25 @@
 const basePath = "../img/hair/";
 const images = [
-  "face", // 1
-  "color", // 2
-  "front_bang_left", // 3
-  "front_bang_right", // 4
-  "front_bang_right2", // 5
-  "left_bang", // 6
-  "left_small_bang2", // 7
-  "left_small_bang3", // 8
-  "middle_front_bang", // 9
-  "small_right_bang", // 10
-  "upper_right_bang", // 11
+  "1",
+  "2",
+  "3",
+  "5",
+  "4",
+  "6",
+  "8",
+  "7",
+  "9",
 ];
 const classes = [
   "class1",
   "class2",
   "class3",
-  "class4",
+ 
   "class5",
+  "class4",
   "class6",
-  "class7",
   "class8",
+  "class7",
   "class9",
   "class10",
   "class11"
@@ -29,7 +28,7 @@ const classes = [
 let html = '';
 
 images.forEach((image, index) => {
-  html += `<img class="w-96 absolute top-0 left-0 ${classes[index]}" src="${basePath}${image}.png" alt="Description of the image">\n`;
+  html += `<img class="w-96 absolute top-0 left-0 ${classes[index]}" src="${basePath}${image}.png" alt="Description of the image" loading="lazy">\n`;
 });
 
 document.getElementById('imageContainer').innerHTML = html;
